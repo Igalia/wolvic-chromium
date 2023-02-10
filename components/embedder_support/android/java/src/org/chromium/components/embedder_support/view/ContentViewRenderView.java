b7a66a98550711d2af4907d0c57b863f58784571
Yeunjoo Choi <ychoi@igalia.com>
2023-02-10 10:42:50 +0900

    Initial implementation for wolvic (experimental)
    - parent : #505125 (JDK11/v8 11.0.211)

--- components/embedder_support/android/java/src/org/chromium/components/embedder_support/view/ContentViewRenderView.java
+++ components/embedder_support/android/java/src/org/chromium/components/embedder_support/view/ContentViewRenderView.java
@@ -36,9 +36,10 @@ import org.chromium.ui.base.WindowAndroid;
 @JNINamespace("embedder_support")
 public class ContentViewRenderView extends FrameLayout {
     // The native side of this object.
-    private long mNativeContentViewRenderView;
-    private WindowAndroid mWindowAndroid;
+    protected long mNativeContentViewRenderView;
+    protected WindowAndroid mWindowAndroid;
 
+    protected SurfaceHolder.Callback mSurfaceCallback;
     protected SurfaceBridge mSurfaceBridge;
     protected WebContents mWebContents;
 
@@ -78,7 +79,7 @@ public class ContentViewRenderView extends FrameLayout {
                 ContentViewRenderViewJni.get().init(ContentViewRenderView.this, rootWindow);
         assert mNativeContentViewRenderView != 0;
         mWindowAndroid = rootWindow;
-        SurfaceHolder.Callback surfaceCallback =
+        mSurfaceCallback =
                 new SurfaceHolder.Callback() {
                     @Override
                     public void surfaceChanged(
@@ -151,7 +152,7 @@ public class ContentViewRenderView extends FrameLayout {
                         }
                     }
                 };
-        mSurfaceBridge.connect(surfaceCallback);
+        mSurfaceBridge.connect(mSurfaceCallback);
     }
 
     @Override
