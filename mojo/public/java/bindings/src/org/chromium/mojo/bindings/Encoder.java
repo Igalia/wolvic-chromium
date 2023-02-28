57f646122c66194776db8d74fe85254c35979374
Miyoung Shin <myid.shin@igalia.com>
2023-02-28 16:36:26 +0900

    Downcast ByteBuffer to Buffer
    This CL solves the java error message, 'java.lang.NoSuchMethodError: No virtual
    method position(I)Ljava/nio/ByteBuffer; in class Ljava/nio/ByteBuffer;'.
    
    In Java 8 used by Wolvic, the class ByteBuffer inherits the position method
    from the class Buffer and has a return type of java.nio.Buffer. On Java 11 and
    lather used by chromium, the method is overridden with an implementation that
    returns java.nio.ByteBuffer.
    
    So, this changes to do explicitly the downcast from the class ByteBuffer to the
    class Buffer for the backward compatibility according to the Java version.

--- mojo/public/java/bindings/src/org/chromium/mojo/bindings/Encoder.java
+++ mojo/public/java/bindings/src/org/chromium/mojo/bindings/Encoder.java
@@ -12,6 +12,7 @@ import org.chromium.mojo.system.Handle;
 import org.chromium.mojo.system.MessagePipeHandle;
 import org.chromium.mojo.system.Pair;
 
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.charset.Charset;
@@ -75,8 +76,8 @@ public class Encoder {
             }
             ByteBuffer newBuffer = ByteBuffer.allocateDirect(targetSize);
             newBuffer.order(ByteOrder.nativeOrder());
-            byteBuffer.position(0);
-            byteBuffer.limit(byteBuffer.capacity());
+            ((Buffer) byteBuffer).position(0);
+            ((Buffer) byteBuffer).limit(byteBuffer.capacity());
             newBuffer.put(byteBuffer);
             byteBuffer = newBuffer;
         }
@@ -93,8 +94,8 @@ public class Encoder {
 
     /** Returns the result message. */
     public Message getMessage() {
-        mEncoderState.byteBuffer.position(0);
-        mEncoderState.byteBuffer.limit(mEncoderState.dataEnd);
+        ((Buffer) mEncoderState.byteBuffer).position(0);
+        ((Buffer) mEncoderState.byteBuffer).limit(mEncoderState.dataEnd);
         return new Message(mEncoderState.byteBuffer, mEncoderState.handles);
     }
 
@@ -327,7 +328,7 @@ public class Encoder {
         byte[] bytes = packBoolsToBitfield(v, 1);
         Encoder encoder = encoderForArrayByTotalSize(bytes.length, v.length, offset);
 
-        encoder.mEncoderState.byteBuffer.position(encoder.mBaseOffset + DataHeader.HEADER_SIZE);
+        ((Buffer) encoder.mEncoderState.byteBuffer).position(encoder.mBaseOffset + DataHeader.HEADER_SIZE);
         encoder.append(bytes);
     }
 
@@ -355,7 +356,7 @@ public class Encoder {
         Encoder encoder =
                 encoderForArrayByTotalSize(
                         hasValueBitfield.length + packed.length, v.length, offset);
-        encoder.mEncoderState.byteBuffer.position(encoder.mBaseOffset + DataHeader.HEADER_SIZE);
+        ((Buffer) encoder.mEncoderState.byteBuffer).position(encoder.mBaseOffset + DataHeader.HEADER_SIZE);
         encoder.append(hasValueBitfield);
         encoder.append(packed);
     }
@@ -602,7 +603,7 @@ public class Encoder {
                         (values.length * elementSizeInByte) + bitField.length,
                         values.length,
                         offset);
-        encoder.mEncoderState.byteBuffer.position(encoder.mBaseOffset + DataHeader.HEADER_SIZE);
+        ((Buffer) encoder.mEncoderState.byteBuffer).position(encoder.mBaseOffset + DataHeader.HEADER_SIZE);
         encoder.mEncoderState.byteBuffer.put(bitField);
         return encoder;
     }
@@ -614,7 +615,7 @@ public class Encoder {
     private Encoder encoderForArrayOfElements(
             int elementSizeInByte, int length, int offset, int expectedLength) {
         Encoder encoder = encoderForArray(elementSizeInByte, length, offset, expectedLength);
-        encoder.mEncoderState.byteBuffer.position(encoder.mBaseOffset + DataHeader.HEADER_SIZE);
+        ((Buffer) encoder.mEncoderState.byteBuffer).position(encoder.mBaseOffset + DataHeader.HEADER_SIZE);
         return encoder;
     }
 
