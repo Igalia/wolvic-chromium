d588fb1855f33063c8bb75864fffffe66c921ec7
Miyoung Shin <myid.shin@igalia.com>
2023-02-28 16:36:26 +0900

    Downcast ByteBuffer to Buffer
    This CL solves the java error message, 'java.lang.NoSuchMethodError: No virtual
    method position(I)Ljava/nio/ByteBuffer; in class Ljava/nio/ByteBuffer;'.
    
    In Java 8 used by Wolvic, the class ByteBuffer inherits the position method
    from the class Buffer and has a return type of java.nio.Buffer. On Java 11 and
    lather used by chromium, the method is overridden with an implementation that
    returns java.nio.ByteBuffer.
    
    So, this changes to do explicitly the downcast from the class ByteBuffer to the
    class Buffer for the backward compatibility according to the Java version.

--- mojo/public/java/bindings/src/org/chromium/mojo/bindings/Encoder.java
+++ mojo/public/java/bindings/src/org/chromium/mojo/bindings/Encoder.java
@@ -10,6 +10,7 @@ import org.chromium.mojo.system.Handle;
 import org.chromium.mojo.system.MessagePipeHandle;
 import org.chromium.mojo.system.Pair;
 
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.charset.Charset;
@@ -72,8 +73,8 @@ public class Encoder {
             }
             ByteBuffer newBuffer = ByteBuffer.allocateDirect(targetSize);
             newBuffer.order(ByteOrder.nativeOrder());
-            byteBuffer.position(0);
-            byteBuffer.limit(byteBuffer.capacity());
+            ((Buffer) byteBuffer).position(0);
+            ((Buffer) byteBuffer).limit(byteBuffer.capacity());
             newBuffer.put(byteBuffer);
             byteBuffer = newBuffer;
         }
@@ -90,8 +91,8 @@ public class Encoder {
 
     /** Returns the result message. */
     public Message getMessage() {
-        mEncoderState.byteBuffer.position(0);
-        mEncoderState.byteBuffer.limit(mEncoderState.dataEnd);
+        ((Buffer) mEncoderState.byteBuffer).position(0);
+        ((Buffer) mEncoderState.byteBuffer).limit(mEncoderState.dataEnd);
         return new Message(mEncoderState.byteBuffer, mEncoderState.handles);
     }
 
