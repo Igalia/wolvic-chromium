a1f9dd2ccef3051b3b955d1c7b058e6f72e63e82
Miyoung Shin <myid.shin@igalia.com>
2023-02-28 18:36:11 +0900

    Add WolvicContentClient and UA methods
    This CL adds WolvicContentClient class and methods of the user
    agent in WolvicContentBrowserClient.

---
+++ wolvic/wolvic_content_client.cc
@@ -0,0 +1,50 @@
+#include "wolvic/wolvic_content_client.h"
+
+#include "base/strings/string_piece.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/values.h"
+#include "build/build_config.h"
+#include "content/shell/grit/shell_resources.h"
+#include "ui/base/l10n/l10n_util.h"
+#include "ui/base/resource/resource_bundle.h"
+
+namespace content {
+
+WolvicContentClient::WolvicContentClient() {}
+
+WolvicContentClient::~WolvicContentClient() {}
+
+std::u16string WolvicContentClient::GetLocalizedString(int message_id) {
+  return l10n_util::GetStringUTF16(message_id);
+}
+
+base::StringPiece WolvicContentClient::GetDataResource(
+    int resource_id,
+    ui::ResourceScaleFactor scale_factor) {
+  return ui::ResourceBundle::GetSharedInstance().GetRawDataResourceForScale(
+      resource_id, scale_factor);
+}
+
+base::RefCountedMemory* WolvicContentClient::GetDataResourceBytes(
+    int resource_id) {
+  return ui::ResourceBundle::GetSharedInstance().LoadDataResourceBytes(
+      resource_id);
+}
+
+std::string WolvicContentClient::GetDataResourceString(int resource_id) {
+  return ui::ResourceBundle::GetSharedInstance().LoadDataResourceString(
+      resource_id);
+}
+
+gfx::Image& WolvicContentClient::GetNativeImageNamed(int resource_id) {
+  return ui::ResourceBundle::GetSharedInstance().GetNativeImageNamed(
+      resource_id);
+}
+
+void WolvicContentClient::AddAdditionalSchemes(Schemes* schemes) {
+#if BUILDFLAG(IS_ANDROID)
+  schemes->local_schemes.push_back(url::kContentScheme);
+#endif
+}
+
+}  // namespace content
