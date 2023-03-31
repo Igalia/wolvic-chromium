eea35c94f2f7b199e958864b24ad75d220b23cf9
Miyoung Shin <myid.shin@igalia.com>
2023-03-31 15:17:30 +0900

    Support fullscreen & media control
    This CL exposes fullscreen and media APIs to Java layer to support fullcreen and media control.
    - Add setWebContentsDelegate in tab jni to enter/exit fullcreen
    - Expose scrubTo(=fast seek)/setMute to Java layer
    - Add a |isActive| parameter to determind the media start/stop status
    - Add //components/browser_ui/media/* in gn to handle the artwork's images

--- content/public/android/java/src/org/chromium/content/browser/MediaSessionImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/MediaSessionImpl.java
@@ -37,6 +37,7 @@ public class MediaSessionImpl extends MediaSession {
 
     private boolean mIsControllable;
     private @Nullable Boolean mIsSuspended;
+    private boolean mIsActive;
     private @Nullable MediaMetadata mMetadata;
     private @Nullable List<MediaImage> mImagesList;
     private @Nullable HashSet<Integer> mActionSet;
@@ -49,7 +50,7 @@ public class MediaSessionImpl extends MediaSession {
     public void addObserver(MediaSessionObserver observer) {
         mObservers.addObserver(observer);
         if (mIsSuspended != null) {
-            observer.mediaSessionStateChanged(mIsControllable, mIsSuspended);
+            observer.mediaSessionStateChanged(mIsControllable, mIsSuspended, mIsActive);
         }
         if (mMetadata != null) {
             observer.mediaSessionMetadataChanged(mMetadata);
@@ -102,6 +103,17 @@ public class MediaSessionImpl extends MediaSession {
     }
 
     @Override
+    public void scrubTo(long millis) {
+        assert millis >= 0 : "Attempted to scrub to a negative posision";
+        MediaSessionImplJni.get().seekTo(mNativeMediaSessionAndroid, MediaSessionImpl.this, millis);
+    }
+
+    @Override
+    public void setMute(boolean mute) {
+        MediaSessionImplJni.get().setMute(mNativeMediaSessionAndroid, MediaSessionImpl.this, mute);
+    }
+
+    @Override
     public void didReceiveAction(int action) {
         MediaSessionImplJni.get()
                 .didReceiveAction(mNativeMediaSessionAndroid, MediaSessionImpl.this, action);
@@ -136,12 +148,15 @@ public class MediaSessionImpl extends MediaSession {
     }
 
     @CalledByNative
-    private void mediaSessionStateChanged(boolean isControllable, boolean isSuspended) {
+    private void mediaSessionStateChanged(
+            boolean isControllable, boolean isSuspended, boolean isActive) {
         mIsControllable = isControllable;
         mIsSuspended = isSuspended;
+        mIsActive = isActive;
 
         for (mObserversIterator.rewind(); mObserversIterator.hasNext(); ) {
-            mObserversIterator.next().mediaSessionStateChanged(isControllable, isSuspended);
+            mObserversIterator.next().mediaSessionStateChanged(
+                    isControllable, isSuspended, isActive);
         }
     }
 
@@ -203,7 +218,8 @@ public class MediaSessionImpl extends MediaSession {
         void seek(long nativeMediaSessionAndroid, MediaSessionImpl caller, long millis);
 
         void seekTo(long nativeMediaSessionAndroid, MediaSessionImpl caller, long millis);
-
+        void scrubTo(long nativeMediaSessionAndroid, MediaSessionImpl caller, long millis);
+        void setMute(long nativeMediaSessionAndroid, MediaSessionImpl caller, boolean mute);
         void didReceiveAction(long nativeMediaSessionAndroid, MediaSessionImpl caller, int action);
 
         void requestSystemAudioFocus(long nativeMediaSessionAndroid, MediaSessionImpl caller);
