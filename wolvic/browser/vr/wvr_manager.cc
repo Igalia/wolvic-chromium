af9b20b18bb7689096ebc72cf6d68bf50ce24f99
Miyoung Shin <myid.shin@igalia.com>
2023-05-08 22:26:19 +0900

    Fixed the white flashing issue and improved the fps
    - Fixed the white flashing issue
      When sharing textures, the following order should be ensured.
    
      Processing (write texture) in the browser
      -> The frame-available callback in the browser
      -> consuming (read texture) in Wolvic
         StartFrame(call UpdateTexImage)
         -> Draw
         -> EndFrame (Call ReleaseTexImage)
    
       Wolvic accesses the last texture every tick(=vsync) even if we
       don't set the next frame id. Then, it can cause to update/release
       of texture in Wolvic when the browser is processing the texture on
       another thread, which can be broken the above sequence. This will
       cause the empty buffer frame in texture and occur a white flashing
       issue.
    
       Reset the texture name as zero after finishing the frame to avoid
       the racing between processing the texture in chromium and consuming
       it in Wolvic.
    
    - Fixed no frame issue when reaching a maximum value of frame ID
      Chromium has a maximum value of frame ID as 255(uint8), and when
      recycling frame id to zero, processing the frame gets stuck.
    
    - Reduced the time out value to 10ms
      If the texture conflicts, the callback could not be called in time.
      We reduce the timer's time so that the next frame is handled shortly.

--- wolvic/browser/vr/wvr_manager.cc
+++ wolvic/browser/vr/wvr_manager.cc
@@ -27,6 +27,8 @@ namespace wolvic {
 
 namespace {
 
+const int64_t kFrameTimeOutMilliseconds = 10;
+
 void WvrMatToTransform(const float in[16], gfx::Transform* out) {
   *out = gfx::Transform::RowMajor(in[0], in[1], in[2], in[3], in[4], in[5],
                                   in[6], in[7], in[8], in[9], in[10], in[11],
@@ -306,19 +308,21 @@ void WvrManager::OnWebXrFrameAvailable() {
   if (!webxr_frame_timeout_closure_.IsCancelled())
     webxr_frame_timeout_closure_.Cancel();
 
-  // Frame should be locked. Unlock it.
-  DCHECK(webxr_->GetProcessingFrame()->state_locked);
-  webxr_->GetProcessingFrame()->state_locked = false;
+  if (webxr_->HaveProcessingFrame()) {
+    // Frame should be locked. Unlock it.
+    DCHECK(webxr_->GetProcessingFrame()->state_locked);
+    webxr_->GetProcessingFrame()->state_locked = false;
 
-  if (!SubmitFrameInternal(webxr_->GetProcessingFrame()->index))
-    return;
+    if (!SubmitFrameInternal(webxr_->GetProcessingFrame()->index))
+      return;
+
+    if (webxr_->HaveRenderingFrame())
+      webxr_->EndFrameRendering();
+    webxr_->TransitionFrameProcessingToRendering();
+  }
 
   // Renderer is waiting for the previous frame to render, unblock it now.
   submit_client_->OnSubmitFrameRendered();
-
-  if (webxr_->HaveRenderingFrame())
-    webxr_->EndFrameRendering();
-  webxr_->TransitionFrameProcessingToRendering();
 
   TryStartAnimatingFrame();
 }
@@ -492,15 +496,21 @@ bool WvrManager::SubmitFrameInternal(int16_t frame_index) {
     externalRect.width = 0.5f;
     externalRect.height = 1.0f;
   }
+
+  last_frame_index_ = frame_index;
   PushState(true);
   PullState([this]() {
-    return (system_state_.displayState.lastSubmittedFrameId >=
+    return (system_state_.displayState.lastSubmittedFrameId ==
             last_frame_index_) ||
            system_state_.displayState.suppressFrames ||
            !system_state_.displayState.isConnected;
   });
 
-  last_frame_index_ = system_state_.displayState.lastSubmittedFrameId;
+  // Avoid racing texture between processing in chromium and consuming in
+  // wolvic.
+  layer.textureHandle = 0;
+  PushState(true);
+
   return true;
 }
 
@@ -562,7 +572,7 @@ void WvrManager::ProcessFrameFromMailbox(int16_t frame_index,
 
   task_runner_->PostDelayedTask(FROM_HERE,
                                 webxr_frame_timeout_closure_.callback(),
-                                base::Milliseconds(100));
+                                base::Milliseconds(kFrameTimeOutMilliseconds));
 }
 
 void WvrManager::SubmitFrameDrawnIntoTexture(int16_t frame_index,
