43a521fedae43238ff3e58aa022c416d1aa8d5b6
Miyoung Shin <124305083+mshin-wolvic@users.noreply.github.com>
2023-06-16 22:56:49 +0900

    Add SelectPopup#Factory (#14)
    * Add SelectPopup#Factory
    
    Co-authored-by: Miyoung Shin <myid.shin@igalia.com>

--- content/public/android/java/src/org/chromium/content/browser/input/SelectPopup.java
+++ content/public/android/java/src/org/chromium/content/browser/input/SelectPopup.java
@@ -13,6 +13,7 @@ import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
 import org.jni_zero.NativeMethods;
 
+import org.chromium.base.Callback;
 import org.chromium.base.UserData;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
@@ -51,6 +52,13 @@ public class SelectPopup
          */
         void hide(boolean sendsCancelMessage);
     }
+
+    public interface Factory {
+        public Ui create(Context windowContext, Callback<int[]> selectionChangedCallback,
+                List<SelectPopupItem> items, boolean multiple, int[] selected);
+    }
+
+    private static Factory sPopupFactory;
 
     private final WebContentsImpl mWebContents;
     private @Nullable View mContainerView;
@@ -125,6 +133,10 @@ public class SelectPopup
         close();
     }
 
+    public static void setFactory(Factory factory) {
+        sPopupFactory = factory;
+    }
+
     /**
      * Called (from native) when the lt&;select&gt; popup needs to be shown.
      * @param anchorView View anchored for popup.
@@ -163,7 +175,10 @@ public class SelectPopup
         for (int i = 0; i < items.length; i++) {
             popupItems.add(new SelectPopupItem(items[i], enabled[i]));
         }
-        if (DeviceFormFactor.isTablet()
+        if (sPopupFactory != null) {
+            mPopupView = sPopupFactory.create(
+                context, this::selectMenuItems, popupItems, multiple, selectedIndices);
+        } else if (DeviceFormFactor.isTablet()
                 && !multiple
                 && !AccessibilityState.isTouchExplorationEnabled()) {
             mPopupView =
