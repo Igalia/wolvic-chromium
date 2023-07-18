9e88f966206e8ae2af58636b0f9d0a4eec97eb7c
Zakhar Voit <zvoit@igalia.com>
2023-07-18 14:35:50 +0700

    Implement User Agent settings
    Add a mechanism of sending Wolvic settings to the Chromium backend and
    also implements support for the User Agent settings. Logic for the User
    Agent support is using the same approach as the gecko backend
    (SessionImpl.java).
    
    Additionally, remove GetUserAgentBasedOnPolicy() method since we're not
    planning to support any enterprise policies and our implementation of
    GetUserAgent() will be used by default.

---
+++ wolvic/browser/session_settings.h
@@ -0,0 +1,43 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WOLVIC_BROWSER_SESSION_SETTINGS_H_
+#define WOLVIC_BROWSER_SESSION_SETTINGS_H_
+
+#include <string>
+
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace content {
+
+enum class UserAgentMode {
+  kMobile = 0,
+  kDesktop = 1,
+  kMobileVR = 2,
+};
+
+// A singleton class holding all settings for the current session.
+class SessionSettings {
+ public:
+  explicit SessionSettings();
+  SessionSettings(const SessionSettings&) = delete;
+  SessionSettings& operator=(const SessionSettings&) = delete;
+  ~SessionSettings();
+
+  // Returns the singleton instance.
+  static SessionSettings* Get();
+
+  void SetUserAgentMode(UserAgentMode value);
+  UserAgentMode GetUserAgentMode();
+  void SetUserAgentOverride(const absl::optional<std::string>& value);
+  absl::optional<std::string> GetUserAgentOverride();
+
+ private:
+  UserAgentMode user_agent_mode_ = UserAgentMode::kMobile;
+  absl::optional<std::string> user_agent_override_;
+};
+
+}  // namespace content
+
+#endif  // WOLVIC_BROWSER_SESSION_SETTINGS_H_
---
+++ wolvic/java/org/chromium/wolvic/SessionSettings.java
@@ -0,0 +1,41 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.wolvic;
+
+import androidx.annotation.Nullable;
+
+import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.NativeMethods;
+
+@JNINamespace("content")
+public class SessionSettings {
+    public SessionSettings() {}
+
+    public void setUserAgentMode(int value) {
+        SessionSettingsJni.get().setUserAgentMode(value);
+    }
+
+    public int getUserAgentMode() {
+        return SessionSettingsJni.get().getUserAgentMode();
+    }
+
+    public void setUserAgentOverride(@Nullable String value) {
+        SessionSettingsJni.get().setUserAgentOverride(value);
+    }
+
+    @Nullable
+    public String getUserAgentOverride() {
+        return SessionSettingsJni.get().getUserAgentOverride();
+    }
+
+    @NativeMethods
+    public interface Natives {
+        void setUserAgentMode(int value);
+        int getUserAgentMode();
+        void setUserAgentOverride(@Nullable String value);
+        @Nullable
+        String getUserAgentOverride();
+    }
+}
