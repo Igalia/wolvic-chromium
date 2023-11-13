85134d13ad44f5a24e7f322e330f96ef84bbb5fa
Zakhar Voit <zvoit@igalia.com>
2023-11-13 12:57:31 +0800

    Remove YoutubeUrlLoaderRequestInterceptor
    Remove the Youtube redirection functionality because it will be
    implemented on the wolvic side.

--- wolvic/wolvic_content_browser_client.h
+++ wolvic/wolvic_content_browser_client.h
@@ -49,12 +49,6 @@ class WolvicContentBrowserClient : public ContentBrowserClient {
 #endif
   void AppendExtraCommandLineSwitches(base::CommandLine* command_line,
                                       int child_process_id) override;
-  std::vector<std::unique_ptr<content::URLLoaderRequestInterceptor>>
-  WillCreateURLLoaderRequestInterceptors(
-      content::NavigationUIData* navigation_ui_data,
-      int frame_tree_node_id,
-      int64_t navigation_id,
-      scoped_refptr<base::SequencedTaskRunner> navigation_response_task_runner) override;
   void BindMediaServiceReceiver(RenderFrameHost *render_frame_host, mojo::GenericPendingReceiver receiver) override;
 
  private:
