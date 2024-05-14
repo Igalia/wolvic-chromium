3867b17e40093d3eb759109d5d64a31291513b88
Zakhar Voit <zvoit@igalia.com>
2024-05-14 14:33:36 +0800

    Revert "Remove ImageTransportSurfaceDelegate"
    https://github.com/Igalia/wolvic-chromium/issues/101
    
    This reverts commit e8d3e3e8e4160d839db5e99bdc3ce08ba0de1074.

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
@@ -1965,7 +1965,7 @@ bool SkiaOutputSurfaceImplOnGpu::InitializeForGL() {
         shared_gpu_deps_->memory_tracker(),
         GetDidSwapBuffersCompleteCallback());
   } else {
-    presenter_ = dependency_->CreatePresenter();
+    presenter_ = dependency_->CreatePresenter(weak_ptr_factory_.GetWeakPtr());
     if (!presenter_) {
       gl::GLSurfaceFormat format;
 #if BUILDFLAG(IS_ANDROID)
@@ -1974,7 +1974,8 @@ bool SkiaOutputSurfaceImplOnGpu::InitializeForGL() {
         format.SetRGB565();
       }
 #endif
-      gl_surface_ = dependency_->CreateGLSurface(format);
+      gl_surface_ =
+          dependency_->CreateGLSurface(weak_ptr_factory_.GetWeakPtr(), format);
       if (!gl_surface_) {
         return false;
       }
@@ -1999,7 +2000,6 @@ bool SkiaOutputSurfaceImplOnGpu::InitializeForGL() {
             shared_gpu_deps_->memory_tracker(),
             GetDidSwapBuffersCompleteCallback(), GetReleaseOverlaysCallback());
 #else   // !BUILDFLAG(IS_WIN)
-        AddChildWindowToBrowser(presenter_->GetWindow());
         output_device_ = std::make_unique<SkiaOutputDeviceDComp>(
             dependency_, shared_image_factory_.get(),
             shared_image_representation_factory_.get(), context_state_.get(),
@@ -2071,7 +2071,7 @@ bool SkiaOutputSurfaceImplOnGpu::InitializeForVulkan() {
   output_presenter =
       OutputPresenterFuchsia::Create(window_surface_.get(), dependency_);
 #else
-  presenter_ = dependency_->CreatePresenter();
+  presenter_ = dependency_->CreatePresenter(weak_ptr_factory_.GetWeakPtr());
   if (presenter_) {
     output_presenter = std::make_unique<OutputPresenterGL>(
         presenter_, dependency_, shared_image_factory_.get(),
@@ -2153,7 +2153,7 @@ bool SkiaOutputSurfaceImplOnGpu::InitializeForDawn() {
   NOTREACHED_NORETURN();
 
 #elif BUILDFLAG(IS_WIN)
-  presenter_ = dependency_->CreatePresenter();
+  presenter_ = dependency_->CreatePresenter(weak_ptr_factory_.GetWeakPtr());
   if (presenter_) {
     AddChildWindowToBrowser(presenter_->GetWindow());
     output_device_ = std::make_unique<SkiaOutputDeviceDComp>(
@@ -2175,7 +2175,7 @@ bool SkiaOutputSurfaceImplOnGpu::InitializeForDawn() {
   return true;
 
 #elif BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
-  presenter_ = dependency_->CreatePresenter();
+  presenter_ = dependency_->CreatePresenter(weak_ptr_factory_.GetWeakPtr());
 
 #if BUILDFLAG(IS_ANDROID)
   if (!presenter_) {
@@ -2222,7 +2222,7 @@ bool SkiaOutputSurfaceImplOnGpu::InitializeForMetal() {
         shared_gpu_deps_->memory_tracker(),
         GetDidSwapBuffersCompleteCallback());
   } else {
-    presenter_ = dependency_->CreatePresenter();
+    presenter_ = dependency_->CreatePresenter(weak_ptr_factory_.GetWeakPtr());
     CHECK(presenter_);
 
 #if BUILDFLAG(IS_MAC)
@@ -2458,6 +2458,11 @@ void SkiaOutputSurfaceImplOnGpu::AddChildWindowToBrowser(
 const gpu::gles2::FeatureInfo* SkiaOutputSurfaceImplOnGpu::GetFeatureInfo()
     const {
   return feature_info_.get();
+}
+
+const gpu::GpuPreferences& SkiaOutputSurfaceImplOnGpu::GetGpuPreferences()
+    const {
+  return gpu_preferences_;
 }
 
 void SkiaOutputSurfaceImplOnGpu::DidSwapBuffersCompleteInternal(
