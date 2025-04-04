224baa193e30091492d9bdc72376420bea356304
Javier Fernández García-Boente <jfernandez@igalia.com>
2025-04-05 01:33:39 +0200

    Rebase M126: Permission status receives the device status

--- wolvic/wolvic_permission_manager.h
+++ wolvic/wolvic_permission_manager.h
@@ -65,7 +65,8 @@ class WolvicPermissionManager : public content::PermissionControllerDelegate {
       const url::Origin& embedding_origin) override;
   blink::mojom::PermissionStatus GetPermissionStatusForCurrentDocument(
       blink::PermissionType permission,
-      content::RenderFrameHost* render_frame_host) override;
+      content::RenderFrameHost* render_frame_host,
+      bool should_include_device_status) override;
   blink::mojom::PermissionStatus GetPermissionStatusForWorker(
       blink::PermissionType permission,
       content::RenderProcessHost* render_process_host,
@@ -79,6 +80,7 @@ class WolvicPermissionManager : public content::PermissionControllerDelegate {
       content::RenderProcessHost* render_process_host,
       content::RenderFrameHost* render_frame_host,
       const GURL& requesting_origin,
+      bool should_include_device_status,
       base::RepeatingCallback<void(blink::mojom::PermissionStatus)> callback)
       override;
   void UnsubscribeFromPermissionStatusChange(
