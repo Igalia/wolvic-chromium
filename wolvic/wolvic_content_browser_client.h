2497688113c14cf86007cab0c5a4d65cc4629775
Javier Fernández García-Boente <jfernandez@igalia.com>
2025-04-20 23:27:37 +0200

    Rebase M130: The CreateLoginDelegate method has a new argument
    The new argument is a boolean value to indicate that the LoginDelegate
    is going to be created for a new navigation request.

--- wolvic/wolvic_content_browser_client.h
+++ wolvic/wolvic_content_browser_client.h
@@ -51,6 +51,7 @@ class WolvicContentBrowserClient : public content::ContentBrowserClient {
       content::BrowserContext* browser_context,
       const content::GlobalRequestID& request_id,
       bool is_request_for_primary_main_frame,
+      bool is_request_for_navigation,
       const GURL& url,
       scoped_refptr<net::HttpResponseHeaders> response_headers,
       bool first_auth_attempt,
