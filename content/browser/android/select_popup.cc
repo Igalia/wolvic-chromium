1392487d08bd9585c0f353f0a02d80d5de2ff248
R800ZZ <admin@vr180g.com>
2026-08-21 15:24:54 +0700

    Fix the bug where the HTML <select> popup does not appear.

--- content/browser/android/select_popup.cc
+++ content/browser/android/select_popup.cc
@@ -1,22 +1,19 @@
 // Copyright 2018 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+#include "base/logging.h" //r800zz
 #include "content/browser/android/select_popup.h"
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
-#include "base/containers/heap_array.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/browser/web_contents/web_contents_view_android.h"
+#include "content/public/android/content_jni_headers/SelectPopup_jni.h"
 #include "ui/gfx/geometry/rect_f.h"
 
-// Must come after all headers that specialize FromJniType() / ToJniType().
-#include "content/public/android/content_jni_headers/SelectPopup_jni.h"
-
 using base::android::AttachCurrentThread;
-using base::android::JavaRef;
+using base::android::JavaParamRef;
 using base::android::ScopedJavaLocalRef;
 
 namespace content {
@@ -66,8 +63,10 @@ void SelectPopup::ShowMenu(
     bool right_aligned) {
   JNIEnv* env = AttachCurrentThread();
   ScopedJavaLocalRef<jobject> j_obj = java_obj_.get(env);
-  if (j_obj.is_null())
+  if (j_obj.is_null()){
+    LOG(ERROR) << "R800ZZ_SELECT: j_obj is null"; //r800zz
     return;
+  }
 
   // Hide the popup menu if the mojo connection is still open.
   if (popup_client_)
@@ -78,32 +77,33 @@ void SelectPopup::ShowMenu(
   // given |selected_item| as is.
   ScopedJavaLocalRef<jintArray> selected_array;
   if (multiple) {
-    auto native_selected_array =
-        base::HeapArray<int32_t>::WithSize(items.size());
+    std::unique_ptr<jint[]> native_selected_array(new jint[items.size()]);
     size_t selected_count = 0;
     for (size_t i = 0; i < items.size(); ++i) {
       if (items[i]->checked)
         native_selected_array[selected_count++] = i;
     }
 
-    selected_array = jni_zero::AdoptRef(env, env->NewIntArray(selected_count));
+    selected_array =
+        ScopedJavaLocalRef<jintArray>(env, env->NewIntArray(selected_count));
     env->SetIntArrayRegion(selected_array.obj(), 0, selected_count,
-                           native_selected_array.data());
+                           native_selected_array.get());
   } else {
-    selected_array = jni_zero::AdoptRef(env, env->NewIntArray(1));
-    int32_t value = selected_item;
+    selected_array = ScopedJavaLocalRef<jintArray>(env, env->NewIntArray(1));
+    jint value = selected_item;
     env->SetIntArrayRegion(selected_array.obj(), 0, 1, &value);
   }
 
-  auto enabled_array = jni_zero::AdoptRef(env, env->NewIntArray(items.size()));
+  ScopedJavaLocalRef<jintArray> enabled_array(env,
+                                              env->NewIntArray(items.size()));
   std::vector<std::string> labels;
   labels.reserve(items.size());
   for (size_t i = 0; i < items.size(); ++i) {
     labels.push_back(items[i]->label.value_or(""));
-    int32_t enabled = (items[i]->type == blink::mojom::MenuItem::Type::kGroup
-                           ? POPUP_ITEM_TYPE_GROUP
-                           : (items[i]->enabled ? POPUP_ITEM_TYPE_ENABLED
-                                                : POPUP_ITEM_TYPE_DISABLED));
+    jint enabled = (items[i]->type == blink::mojom::MenuItem::Type::kGroup
+                        ? POPUP_ITEM_TYPE_GROUP
+                        : (items[i]->enabled ? POPUP_ITEM_TYPE_ENABLED
+                                             : POPUP_ITEM_TYPE_DISABLED));
     env->SetIntArrayRegion(enabled_array.obj(), i, 1, &enabled);
   }
   ScopedJavaLocalRef<jobjectArray> items_array(
@@ -111,19 +111,32 @@ void SelectPopup::ShowMenu(
   ui::ViewAndroid* view = web_contents_->GetNativeView();
   popup_view_ = view->AcquireAnchorView();
   const ScopedJavaLocalRef<jobject> popup_view = popup_view_.view();
+/* r800zz wolvic-chromium SELECT POPUP bugfix delete
   if (popup_view.is_null())
     return;
+*/
+  //r800zz wolvic-chromium SELECT POPUP bug fix add
+  // Wolvic uses its own SelectPopup.Factory UI and does not require an Android
+  // anchor View. //r800zz
+  if (!popup_view.is_null()) { //r800zz
+    gfx::RectF bounds_dip = gfx::RectF(bounds); //r800zz
+    bounds_dip.Scale(1 / web_contents_->GetNativeView()->GetDipScale()); //r800zz
+    view->SetAnchorRect(popup_view, bounds_dip); //r800zz
+  } //r800zz
+//
 
   popup_client_.Bind(std::move(popup_client));
   popup_client_.set_disconnect_handler(
       base::BindOnce(&SelectPopup::HideMenu, base::Unretained(this)));
 
+/* r800zz wolvic-chromium SELECT POPUP fix delete 
   // |bounds| is in physical pixels.
   gfx::RectF bounds_dip = gfx::RectF(bounds);
   bounds_dip.Scale(1 / web_contents_->GetNativeView()->GetDipScale());
   view->SetAnchorRect(popup_view, bounds_dip);
+*/
   Java_SelectPopup_show(
-      env, j_obj, popup_view, reinterpret_cast<int64_t>(popup_client_.get()),
+      env, j_obj, popup_view, reinterpret_cast<jlong>(popup_client_.get()),
       items_array, enabled_array, multiple, selected_array, right_aligned);
 }
 
@@ -137,13 +150,14 @@ void SelectPopup::HideMenu() {
 }
 
 void SelectPopup::SelectMenuItems(JNIEnv* env,
-                                  int64_t selectPopupDelegate,
-                                  const JavaRef<jintArray>& indices) {
+                                  const JavaParamRef<jobject>& obj,
+                                  jlong selectPopupDelegate,
+                                  const JavaParamRef<jintArray>& indices) {
   blink::mojom::PopupMenuClient* popup_client_raw_ptr =
       reinterpret_cast<blink::mojom::PopupMenuClient*>(selectPopupDelegate);
   DCHECK(popup_client_raw_ptr && popup_client_.get() == popup_client_raw_ptr);
 
-  if (indices.is_null()) {
+  if (indices == NULL) {
     popup_client_->DidCancel();
     return;
   }
@@ -154,5 +168,3 @@ void SelectPopup::SelectMenuItems(JNIEnv* env,
 }
 
 }  // namespace content
-
-DEFINE_JNI(SelectPopup)
